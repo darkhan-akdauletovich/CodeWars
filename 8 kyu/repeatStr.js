973a3f8f7c241e84fb7dfbe9452dcd2bb57d12a9
darkhan-akdauletovich <d.bolatov2402@gmail.com>
2023-04-10 14:21:06 +0600

    CodeWars

--- 8 kyu/repeatStr.js
+++ 8 kyu/repeatStr.js
@@ -1,5 +1,6 @@
 //Write a function that accepts an integer n and a string s as parameters, and returns a string of s repeated exactly n times.
 
-function repeatStr (n, s) {
-    return '';
-  }
+const repeatStr = (n, s) => s.repeat(n)
+
+//The repeat() method constructs and returns a new string which contains the specified number of copies of the string on which it was called, concatenated together.
+  
