2cbea863370bbc289d65b3e67020ff1a33e3f5a7
darkhan-akdauletovich <d.bolatov2402@gmail.com>
2023-04-13 15:20:49 +0600

    codeWars

--- 7 kyu/add.js
+++ 7 kyu/add.js
@@ -6,6 +6,39 @@
 
 // You may assume both integers are positive integers.
 
-function add(num1, num2) {
-    return num1 + num2;
+// const add = (num1, num2) => {
+//     let arr1 = [...num1.toString()].map(Number).reverse();
+//     let arr2 = [...num2.toString()].map(Number).reverse();
+//     let sum = arr1.map(function (num, idx) {
+//       return num + arr2[idx]
+//     })
+//     return sum
+//   }
+
+const leftPad = (one, two)=>{
+  if(one.length === two.length){
+    return [one, two]
+  } else if(one.length > two.length){
+    const paddedTwo = two.padStart(one.length, '0');
+    return [one, paddedTwo]
+  } else {
+    const paddedOne = one.padStart(two.length, '0');
+    return [paddedOne, two]
   }
+}
+function add(num1, num2) {
+  const padded = leftPad(num1.toString(), num2.toString())
+  let final = '';
+  let paddedOne = padded[0].split('');
+  padded[1].split('').map( (c, i)=>{
+     let int = ( parseInt(padded[1][i]) + parseInt(paddedOne[i]) ).toString();
+     final += int
+   })
+  return parseInt(final);
+}
+
+// String.prototype.padStart()
+// The padStart() method pads the current string with another string (multiple times, if needed) until the resulting string reaches the given length. The padding is applied from the start of the current string.
+
+// String.prototype.split()
+// The split() method takes a pattern and divides a String into an ordered list of substrings by searching for the pattern, puts these substrings into an array, and returns the array.
