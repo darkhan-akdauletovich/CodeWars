60970e7bdb3876178c428e95d331dfa9a732a1d1
darkhan-akdauletovich <d.bolatov2402@gmail.com>
2023-04-25 17:21:49 +0600

    codeWars

---
+++ 7 kyu/maxTriSum.js
@@ -0,0 +1,15 @@
+// Task
+// Given an array/list [] of n integers , find maximum triplet sum in the array Without duplications .
+
+// Notes :
+// Array/list size is at least 3 .
+
+// Array/list numbers could be a mixture of positives , negatives and zeros .
+
+// Repetition of numbers in the array/list could occur , So (duplications are not included when summing).
+
+// Input >> Output Examples
+// 1- maxTriSum ({3,2,6,8,2,3}) ==> return (17)
+const maxTriSum = numbers => [...new Set(numbers.sort((a,b) => b - a))].slice(0, 3).reduce((a,b) => a + b)
+
+console.log(maxTriSum([3,2,6,8,2,3]))
