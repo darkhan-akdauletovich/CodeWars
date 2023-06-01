3b25981894ff27d0824b4aa847877ee5498c6d25
darkhan-akdauletovich <d.bolatov2402@gmail.com>
2023-06-01 12:40:51 +0600

    codeWars

--- test.js
+++ test.js
@@ -1,32 +1,22 @@
-console.log(1);
-let promise3
-setTimeout(()=> {
-    console.log(2)
-    promise3 = new Promise(resolve => {
-        console.log(9);
-        resolve(10);
-    })
-}, 2000);
-
-setTimeout(()=> {
-    console.log(8)
-}, 1000);
-
-const promise1= new Promise(resolve => {
-    console.log(3);
-    resolve(4);
-});
-
-const promise2 = new Promise(resolve => {
-    console.log(5);
-    resolve(6)
-});
-
-promise1.then(console.log);
-promise2.then(console.log);
-
-console.log(7);
-// 1, 7, 3 , 4, 5, 6, 2
-//1, 3, 5, 7, 4, 6, 2
-//1, 3, 5, 4, 5, 6, 7, 8, 2, 9, 10
-// 1, 3, 5, 7, 4, 6, 8, 2, 9, 10
+// const lines = 6;
+// let res =''
+// for (let i = 0; i <= lines; i++){
+//     for ( let j = 0; j <= lines-i; j++){
+//         res+=" ";
+//     }
+//         for(let j=0;j<2*i+1;j++){
+//         res += "*"
+//         }
+//         res += "\n" 
+// }
+// console.log(res)
+let res = ''
+for (let i=5; i>=0; i--){
+    for (let j=0; j<i; j++){
+        res += '*' 
+        // console.log(1);
+    }
+    
+    console.log(res);
+    res = ''
+}
